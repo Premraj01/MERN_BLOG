c6e280d0c3e563a79bad63f0fc716f97e3d948c3
Premraj01 <premrajtripute777@gmail.com>
2020-10-08 20:07:06 +0530

    Server commit

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 
 const express = require("express");
 const connectDB = require("./config/db");
-const cors = require("cors")
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 //Connect Database
@@ -10,7 +11,7 @@ connectDB();
 
 //Init Middleware
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 //Define Route
 app.use("/api/users", require("./routes/api/users"));
@@ -18,7 +19,14 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
-app.get("/", (req, res) => res.send("API Running"));
+// Serve static assests in Production
+if (process.env.NODE_ENV === "production") {
+  //set Static Folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT} `));
